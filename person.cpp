3eb2a4fbceb48c4f070a428fd5049a5ad21dc299
Sesen8 <yonass@spu.edu>
2022-05-02 17:45:26 -0700

    person is done

--- person.cpp
+++ person.cpp
@@ -6,6 +6,20 @@
  */
 #include "person.h"
 #include "date.h"
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+using std::string;
+using std::stringstream;
+using std::istream;
+using std::ostream;
+using std::cout;
+using std::endl;
+
+
 
 Person::Person(const string &name, const Date &birthday) :_name(name), _birthday(birthday){
 
@@ -29,11 +43,32 @@ string Person::ToString() const {
 
 void Person::Read(istream &input) {
 
+    string line;
+    getline(input,line);
+    stringstream cutUp(line);
+
+
+    string name;
+    size_t year;
+    size_t month;
+    size_t day;
+
+    cutUp >> name >> year >> month >> day;
+
+    _name=name;
+    Date newDate(year,month,day);
+    _birthday = newDate;
 
 }
 
 void Person::Write(ostream &output) const {
 
+    string name1 = _name;
+    size_t year1 = _birthday.GetYear();
+    size_t month1 = _birthday.GetMonth();
+    size_t day1 = _birthday.GetDay();
+
+    output << name1 << " " << year1 << " " << month1 << " " << day1;
 
 
 }
